7e356943e4c141fac84b4decf2741b0098b4e965
anushridixit <anushridixit@ucla.edu>
2026-06-10 02:28:55 +0000

    Deploying to gh-pages from @ practice-lab-ucla/practice-lab-ucla.github.io@1794ccdcbb1408086c013668bf1901082b0d22cf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,12 +423,12 @@ ninja.data = [{
               window.location.href = "/people/Jacob_Pham/";
             },},{id: "people-lucas-schweighofer",
           title: 'Lucas Schweighofer',
-          description: "Undergraduate student in Mechanical Engineering",
+          description: "Mechanical Engineering",
           section: "People",handler: () => {
               window.location.href = "/people/Lucas_Schweighofer/";
             },},{id: "people-sanjit-sarda",
           title: 'Sanjit Sarda',
-          description: "Undergraduate student in Electrical and Computer Engineering",
+          description: "Electrical and Computer Engineering",
           section: "People",handler: () => {
               window.location.href = "/people/Sanjit_Sarda/";
             },},{id: "people-shoh-nishino",
